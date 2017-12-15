dbbb1ea252f4cc350314f9f9ca721d2b6cd29013
Jeffrey Dodge <Jeff@Jeffreys-iMac.local>
2017-12-15 16:50:59 -0700

    Updated all header/footer to be bootstrap, fixed main carousel for responsiveness

--- contact.php
+++ contact.php
@@ -8,16 +8,10 @@
 	<link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
 	<!--header font-->
 	<link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
-	<!--image slider-->
-	<link href="_scripts/themes/4/js-image-slider.css" rel="stylesheet" type="text/css" />
-		<script src="_scripts/themes/4/js-image-slider.js" type="text/javascript"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<!--Include this line in your HTML-->
 	<link rel="stylesheet" href="_scripts/lightbox2-master/css/lightbox.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 	<?php
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -44,7 +38,7 @@
 
 <body>
         <!--includes header from header file-->
-        <?php include("header.php"); ?>
+        <header></header>
         <div class="section-holder contact-section-holder">
 <section class="main-section">
         <h1>[ Contact Me ]</h1>
@@ -102,7 +96,15 @@
     </section>
     </div>
     <!--includes footer from footer file-->
-    <?php include("footer.php"); ?>
+    <footer></footer>
     <!--Include this line in your HTML HERE-->
+
+	
+	<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" crossorigin="anonymous"></script>
+		<script src="./_scripts/main.js"></script>
 </body>
 </html>
