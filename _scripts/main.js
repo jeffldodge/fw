7104b2474564b598833ad50887c5a17d1c2c23bb
Jeffrey Dodge <Jeff@Jeffreys-iMac.local>
2017-12-15 17:54:27 -0700

    Responsive menu fixes

--- _scripts/main.js
+++ _scripts/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     $.get('./header.html',function(data){
         $('header').html(data);
+        var selected=$("header [href~='"+window.location.pathname.replace("/","")+"']");
+        selected.closest(".nav-item").addClass("selected")
+        if(selected.hasClass("dropdown-item"))
+        {
+            selected.addClass("selected");
+        }
     })
     $.get('./footer.html',function(data){
         $('footer').html(data);
